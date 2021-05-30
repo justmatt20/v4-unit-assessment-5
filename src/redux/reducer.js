e8b7596142cb4f64229649659e5fed7c9e809e47
Justine Matthews <justinematt14@gmail.com>
2021-05-30 15:39:20 -0400

    one save

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,32 @@
 initialState: { 
-    
+    username: ''
+    password: ''
 }
+const ACTION_TYPE = ACTION_TYPE
+const LOGOUT = LOGOUT
+
+export const updateUser = (initialState) => {
+    const action = {
+        type: ACTION_TYPE,
+        payload: initialState
+    }
+}
+
+export const logoutUser = (initialState) => {
+    const action = {
+        type: LOGOUT,
+        payload: initialState
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    console.log(action)
+    switch(action.type) {
+        case ACTION_TYPE:
+            return [...state];
+        case LOGOUT:
+            return state = ''
+}
+}
+
+export default reducer;
